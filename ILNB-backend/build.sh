f92b59a5ac4cca2b6d0423bdea594cf40613de76
Nitish Kumar <iamnitishsah12@gmail.com>
2025-04-05 04:10:45 +0530

    Create build.sh

---
+++ ILNB-backend/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations and collect static
+python manage.py migrate
+python manage.py collectstatic --noinput
